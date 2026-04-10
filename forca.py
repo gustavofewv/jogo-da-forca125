8212ae2bfdc68455e7c9f336de493bb34924f4d8
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2026-04-10 11:51:46 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,66 +1,66 @@
-import random as rd 
+import random as rd
 
 def mensagem_inicial():
-        print("--------------------------")
-        print("bem vindo ao jogo")
-        print("--------------------------")  
+    print("--------------------------")
+    print("bem vindo ao jogo")
+    print("--------------------------")
 
 def seleciona_palavra_aleatoria():
-
-
     arquivo = open("jogos/palavra.txt","r")
     palavra = []
-    
+
     for linha in arquivo:
         linha = linha.strip()
         palavra.append(linha)
 
-    arquivo.close()
-    posicao = rd.randrange(8,len(palavra))    
-
-    lista = ["--------------------------"]
-
+    arquivo.closed()
+    posicao = rd.randrange(0,len(palavra))
 
     palavra_secreta = palavra[posicao].lower()
     return palavra_secreta
 
-def letras_corretas(palavra_secreta): 
-    return ["_" for letra in palavra_secreta]
+def letras_corretas(palavra_secreta):
+    return ["_" for lrtra in palavra_secreta]
 
-def entradas_dados():
-            
+def entrada_dados():
     chute = input("escreveu uma letra:")
     chute = chute.strip().upper()
     return chute
 
+def chute(palavra_secreta, chute,letras_acertadas):
+    index = 0
+    for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+            return
 
 def jogar_forca():
 
     mensagem_inicial()
     palavra_secreta = seleciona_palavra_aleatoria()
 
-
     letras_acertadas = letras_acertadas
+    
+    
     perdeu = False
     acertou = False
     erros = 0
 
-
     while(not perdeu and not acertou):
-        chute = entradas_dados()
-        index = 0
+        chute = entrada_dados()
+    
         if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
-                    letras_acertadas[index] = letra
-                index = index + 1
-            else:
-                erros = erros + 1 
+             chute_coreto(palavra_secreta, chute,letras_acertadas)
+        
 
-                perdeu = erros == + 6
-                
+        else:
+            erros = erros + 1
+            
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
+
         print(letras_acertadas)
 
-
 if(__name__ == "__main__"):
-    jogar_forca()                
+    jogar_forca()
