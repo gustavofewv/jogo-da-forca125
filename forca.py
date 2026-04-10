11710302edaa20c79fe4cfe42b7b6ad9d9e0c73d
gustavofewv <gustavo.oliveira.alcantara@escola.pr.gov.br>
2026-04-10 11:39:44 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,21 +1,53 @@
-def jogar_forca():
-    print("--------------------------")
-    print("bem vindo ao jogo")
-    print("--------------------------")    
+import random as rd 
+
+def mensagem_inicial():
+        print("--------------------------")
+        print("bem vindo ao jogo")
+        print("--------------------------")  
+
+def seleciona_palavra_aleatoria():
+
+
+    arquivo = open("jogos/palavra.txt","r")
+    palavra = []
+    
+    for linha in arquivo:
+        linha = linha.strip()
+        palavra.append(linha)
+
+    arquivo.close()
+    posicao = rd.randrange(8,len(palavra))    
 
     lista = ["--------------------------"]
 
-    palavra_secreta = "peocessador"
-    letras_acertadas = ["_","_","_","_","_","_","_","_",]
+
+    palavra_secreta = palavra[posicao].lower()
+    return palavra_secreta
+
+def letras_corretas(palavra_secreta): 
+    return ["_" for letra in palavra_secreta]
+
+def entradas_dados():
+            
+    chute = input("escreveu uma letra:")
+    chute = chute.strip().upper()
+    return chute
+
+
+def jogar_forca():
+
+    mensagem_inicial()
+    palavra_secreta = seleciona_palavra_aleatoria()
+
+
+    letras_acertadas = letras_acertadas
     perdeu = False
     acertou = False
     erros = 0
 
 
-    if(not perdeu and not acertou):
-        chute = input("escreveu uma letra:")
-        chute = chute.strip()
-
+    while(not perdeu and not acertou):
+        chute = entradas_dados()
         index = 0
         if(chute in palavra_secreta):
             for letra in palavra_secreta:
